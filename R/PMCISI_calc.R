66b8064a773a3896004c990976b3ef36a6ce6d75
ElizabethArangoR <119826956+ElizabethArangoRuda@users.noreply.github.com>
2025-04-01 21:58:09 -0400

    Trying to commit readme file

---
+++ R/PMCISI_calc.R
@@ -0,0 +1,62 @@
+#' Peat Spread Rate Index
+#'
+#' The PMCISI combines the ISI and PMC to provide a representation of
+#' above-ground fire spread potential in peatland fuels.
+#'
+#' @param input_data A dataframe containing the columns 'Date', 'PMC' and 'ISI'.
+#' @param year_to_plot The year to plot.
+#'
+#' @return A dataframe that retains the original columns and adds a column with the calculated PMC_ISI.
+#' @export
+#'
+#' @examples
+#' input_data <- data.frame(Date = as.Date(c("2014-01-01", "2014-01-02",
+#' "2014-01-03")) , PMC = c(10.005600, 9.686799, 9.688504),
+#' ISI = c(0.2, 0.1, 0.1))
+#' PMCISI_calc(input_data, year_to_plot = "2014")
+PMCISI_calc <- function(input_data, year_to_plot=NULL){
+
+  input_data <- dplyr::mutate(input_data,
+                              PMC_ISI = PMC*ISI,
+                              PMC_ISI_scaled = PMC*ISI/12) # Calculate PMCi as PMC * ISI / 12 so it roughly scales to between 10 and 100
+
+  # Visuals
+  if (!is.null(year_to_plot)){
+    input_data <-  dplyr::filter(input_data, lubridate::year(Date)== year_to_plot)
+    message(paste("Plotting data for year:", year_to_plot))
+  }
+
+  plot <-
+    ggplot2::ggplot(input_data, ggplot2::aes(x=Date, y=PMC_ISI_scaled)) +
+    ggplot2::geom_line()+
+    ggplot2::geom_point()+
+    ggplot2::scale_x_date(
+      date_labels = "%b",        # Display month and day (e.g., "Jan 01")
+      date_breaks = "1 month",         # Set the break interval to 1 day
+      expand = c(0.004, 0.004)
+    ) +
+    ggplot2::labs(x="Date", y = "PMC_ISI")+
+    ggplot2::theme(
+      panel.background = ggplot2::element_rect(fill = "white", colour = "black"),
+      axis.text.x = ggplot2::element_text(angle = 0, size = 14, hjust = -0.5, color = "black"),
+      axis.text.y = ggplot2::element_text(size = 18, colour = "black"),
+      axis.title.x = ggplot2::element_blank(),
+      legend.direction = "horizontal",
+      legend.text = ggplot2::element_text(size = 14),
+      legend.title = ggplot2::element_blank(),
+      legend.background = ggplot2::element_blank(),
+      legend.key.size = ggplot2::unit(0.8, "cm"),
+      legend.key.width = ggplot2::unit(1, "cm"),
+      legend.key = ggplot2::element_rect(colour = NA, fill = NA),
+      axis.title.y = ggplot2::element_text(size = 18, colour = "black"),
+      plot.title = ggplot2::element_text(hjust = 0.01, vjust = -8, size = 18),
+      plot.margin = ggplot2::margin(0.1, 0.2, 0.1, 0.2, "cm")
+    )
+
+  # ---- Display Combined Plot ----
+  print(plot)
+
+  # ---- Return PMCi data ----
+  return(input_data)
+
+}
---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Mildred Lake Dataset
+#'
+#' A dataset containing daily weather observations for Mildred Lake.
+#'
+#' @format A tibble with 730 rows and 5 variables:
+#' \describe{
+#'   \item{Date}{Date of the observation (YYYY-MM-DD)}
+#'   \item{Tmin}{Minimum daily temperature (°C)}
+#'   \item{Tmax}{Maximum daily temperature (°C)}
+#'   \item{PPT}{Daily precipitation (cm)}
+#'   \item{ISI}{Initial Spread Index, a measure of fire spread potential}
+#' }
+#'
+#' @source Simulated or collected from a weather station at Mildred Lake.
+#' data("Mildred_Lake_Data")
+"Mildred_Lake_Data"
